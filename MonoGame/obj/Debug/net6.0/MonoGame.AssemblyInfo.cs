4eef4e3d5be5edacfdad2b12da081ec8c0e21f40
Leonardo de Andrade <leonardo.andrade@actionlabs.com.br>
2024-05-31 17:49:19 -0300

    refactor: Update MonoGame assembly version and source links
    This commit updates the MonoGame assembly version and source links in the project. The assembly version is updated to "1.0.0+ff525dbe7a25a6681c8c0da649e0c3048426b056" in the MonoGame.AssemblyInfo.cs file. Additionally, the source link in the MonoGame.sourcelink.json file is updated to point to the commit "ff525dbe7a25a6681c8c0da649e0c3048426b056" on the GitHub repository. These changes ensure that the correct version and source code are referenced in the project.

--- MonoGame/obj/Debug/net6.0/MonoGame.AssemblyInfo.cs
+++ MonoGame/obj/Debug/net6.0/MonoGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonoGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+616e2a347cdccab0421a61c2ae2a01c61ba28ec1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff525dbe7a25a6681c8c0da649e0c3048426b056")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonoGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonoGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
